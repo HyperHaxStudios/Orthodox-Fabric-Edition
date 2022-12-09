84d92e0379009107825112e060788f31d372d52a
HyperHaxStudios <hypersonic1011@gmail.com>
2022-12-08 19:46:04 -0500

    Initial commit

---
+++ src/main/java/com/hyperhaxstudios/orthodox/Orthodox.java
@@ -0,0 +1,15 @@
+package com.hyperhaxstudios.orthodox;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Orthodox implements ModInitializer {
+	public static final String MOD_ID = "orthodox";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		
+	}
+}
