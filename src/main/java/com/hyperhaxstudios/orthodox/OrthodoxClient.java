3a0f0790bff32c10e98fd9975fb09bf7bbdde40c
HyperHaxStudios <hypersonic1011@gmail.com>
2023-06-14 17:05:02 -0400

    Source Code is back!

---
+++ src/main/java/com/hyperhaxstudios/orthodox/OrthodoxClient.java
@@ -0,0 +1,10 @@
+package com.hyperhaxstudios.orthodox;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OrthodoxClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/hyperhaxstudios/orthodox/OrthodoxDataGenerator.java
@@ -0,0 +1,11 @@
+package com.hyperhaxstudios.orthodox;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class OrthodoxDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
